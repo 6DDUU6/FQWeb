0f7411b4002083955ea7d61fb9b0addec4eb9ce2
fengyuecanzhu <1021300691@qq.com>
2023-06-02 22:57:22 +0800

    Bump to v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "me.fycz.fqweb"
         minSdk = 24
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
